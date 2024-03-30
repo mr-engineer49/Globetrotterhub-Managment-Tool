fd174b865a6a6cbfb751ad252325437159b3cf1b
hysa99 <eneahysa4@gmail.com>
2024-03-30 18:02:54 +0100

    added new changes and features

--- globetrotterhub/settings.py
+++ globetrotterhub/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+6+ewlug-p&$bdr+nuu=d7$!vd^r$+4lfad1t^*=5x0a_^sd(_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*''.vercel.app']
+ALLOWED_HOSTS = ['*', '.vercel.app', '.now.sh']
 
 
 LOGIN_URL = '/login/'
@@ -95,7 +95,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'globetrotterhub.wsgi.app'
+WSGI_APPLICATION = 'globetrotterhub.wsgi.application'
 
 
 # Database
@@ -144,12 +144,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = BASE_DIR / 'img'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
 
 
 # Default primary key field type
